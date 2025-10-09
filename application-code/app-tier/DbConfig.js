062a341315e36cdbb227029d90211e985a16d8c6
tumi-oba <tumidluna@yahoo.com>
2025-10-09 18:19:51 -0500

    adding ngini

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.c12cggm2uyuw.us-east-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'adminstrator',
+    DB_DATABASE : 'webappdb',
+    DB_PORT : 3306
 });
